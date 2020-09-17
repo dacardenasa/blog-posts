4543d2e0a64a036c5f6b9bcc3eeb0a592212a3a9
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-17 10:00:58 -0500

    Updates

---
+++ precache-manifest.2026d6138121cd0146fbefe3827f80a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859ad86af21aa75fbf365287ae9d6d9d",
+    "url": "/blog-posts/index.html"
+  },
+  {
+    "revision": "fef3c734fcff27685a0d",
+    "url": "/blog-posts/static/css/main.aa7818d4.chunk.css"
+  },
+  {
+    "revision": "f4ed1759d3ed5e57e504",
+    "url": "/blog-posts/static/js/2.2ba4feb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog-posts/static/js/2.2ba4feb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef3c734fcff27685a0d",
+    "url": "/blog-posts/static/js/main.456f320c.chunk.js"
+  },
+  {
+    "revision": "5ec9e7f9548e3b74f298",
+    "url": "/blog-posts/static/js/runtime-main.10b261a3.js"
+  },
+  {
+    "revision": "bf15c640e055e713aeb9e18f8dc29747",
+    "url": "/blog-posts/static/media/wave.bf15c640.svg"
+  }
+]);
